c4bcec81fe19e47ad5ef2540749462d094da37e0
Jaideep Yadav <jaideepyadav611@gmail.com>
2025-07-14 16:53:00 +0530

    Time: 3 ms (61.13%) | Memory: 15.4 MB (31.96%) - LeetSync

--- 800-letter-case-permutation/letter-case-permutation.cpp
+++ 800-letter-case-permutation/letter-case-permutation.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-    void solve(int index,string &temp,string &s,vector<string>&ans){
+    void solve(int index,string &temp,string s,vector<string>&ans){
         if(index==s.size()){
             ans.push_back(temp);
             return;
         }
+        if(isalpha(s[index])){
+        string a1=temp;
+        string a2=temp;
+        a1.push_back(tolower(s[index]));
+        a2.push_back(toupper(s[index]));
+        solve(index+1,a1,s,ans);
+        solve(index+1,a2,s,ans);}
         if(isdigit(s[index])){
-            temp.push_back(s[index]);
-            solve(index+1,temp,s,ans);
-            temp.pop_back();
-        }
-        else if(isalpha(s[index])){
-            temp.push_back(tolower(s[index]));
-            solve(index+1,temp,s,ans);
-            temp.pop_back();
-            
-            temp.push_back(toupper(s[index]));
-            solve(index+1,temp,s,ans);
-            temp.pop_back();
+            string a=temp;
+            a.push_back(s[index]);
+            solve(index+1,a,s,ans);
         }
     }
     vector<string> letterCasePermutation(string s) {
-        vector<string>ans;
         string temp="";
+        vector<string>ans;
         solve(0,temp,s,ans);
         return ans;
     }
