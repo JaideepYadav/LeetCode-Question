cf79d966b71a3d4c9dbbb9aac1f7bf988e5de0be
Jaideep Yadav <jaideepyadav611@gmail.com>
2025-07-20 16:01:07 +0530

    Time: 2677 ms (5.10%) | Memory: 16.7 MB (42.03%) - LeetSync

--- 611-valid-triangle-number/valid-triangle-number.cpp
+++ 611-valid-triangle-number/valid-triangle-number.cpp
@@ -5,9 +5,9 @@ public:
         int k=nums.size()-1;
         int count=0;
         for(k;k>=2;k--){
-            int i=0,j=i+1;
+            int i=0;
             while(i<k-1){
-                j=i+1;
+                int j=i+1;
                 while(j<k){
                     if((nums[i]+nums[j])>nums[k])count++;
                     j++;
