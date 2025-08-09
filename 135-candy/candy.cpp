9e59fb0c97ac8b196ebfce2d269198a5bf3b211a
Jaideep Yadav <jaideepyadav611@gmail.com>
2025-08-09 23:32:21 +0530

    Time: 0 ms (100.00%) | Memory: 22.9 MB (43.17%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -6,7 +6,7 @@ public:
         vector<int>arr(n,1);
         for(int i=1;i<n;i++){
             if((ratings[i]>ratings[i-1])){
-                arr[i]=(arr[i-1]+1);
+                arr[i]=arr[i-1]+1;
             }
         }
         for(int i=n-2;i>=0;i--){
