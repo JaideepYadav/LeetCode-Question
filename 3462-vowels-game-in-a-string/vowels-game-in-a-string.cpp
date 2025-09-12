1743e14481f1d82e9fbf592b36630394a59f3607
Jaideep Yadav <jaideepyadav611@gmail.com>
2025-09-12 10:20:27 +0530

    Time: 11 ms (34.33%) | Memory: 21.6 MB (5.41%) - LeetSync

---
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isVowel(char ch) {
+    ch = tolower(ch); // Convert to lowercase to handle both cases
+    return (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u');
+}
+    bool doesAliceWin(string s) {
+        int n=s.size();
+        int index,count=0;
+        vector<int>ans;
+        for(int i=0;i<n;i++){
+            if(isVowel(s[i])){
+                ans.push_back(i);
+                count++;
+            }
+        }
+       // if(count%2!=0 && index==n-1)return true;
+       if(count==0)return false;
+        if(count%2!=0)return true;
+        return true;
+    }
+};
