2f2d3a6acfef66d1bcb8449c46e4a5bc935388ae
Jaideep Yadav <jaideepyadav611@gmail.com>
2025-09-27 21:30:02 +0530

    Time: 7 ms (13.55%) | Memory: 10.5 MB (34.38%) - LeetSync

---
+++ 830-largest-triangle-area/largest-triangle-area.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    double largestTriangleArea(vector<vector<int>>& points) {
+        double Max=0.0;
+        for(int i=0;i<points.size();i++){
+            for(int j=i+1;j<points.size();j++){
+                for(int k=j+1;k<points.size();k++){
+                    double x1=points[i][0];
+                    double y1=points[i][1];
+                    double x2=points[j][0];
+                    double y2=points[j][1];
+                    double x3=points[k][0];
+                    double y3=points[k][1];
+
+                    double a=hypot(x2-x1,y2-y1);
+                    double b=hypot(x2-x3,y2-y3);
+                    double c=hypot(x3-x1,y3-y1);
+
+                    double s=(a+b+c)/2;
+                    double heron=sqrt(s*(s-a)*(s-b)*(s-c));
+                    double shoe=abs(x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2))/2;
+                    Max=max(shoe,Max);
+                }
+            }
+        }
+        return Max;
+    }
+};
